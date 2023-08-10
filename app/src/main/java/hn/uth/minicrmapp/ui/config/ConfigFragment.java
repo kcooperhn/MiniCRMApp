caea0fcf19a59fa399edcb154657a6002be32f8d
kcooperhn <134466354+kcooperhn@users.noreply.github.com>
2023-08-10 11:44:46 -0600

    Uso de cámara
    Agregando uso de cámara para tomar fotografía de perfil del usuario.

--- app/src/main/java/hn/uth/minicrmapp/ui/config/ConfigFragment.java
+++ app/src/main/java/hn/uth/minicrmapp/ui/config/ConfigFragment.java
@@ -1,6 +1,14 @@
 package hn.uth.minicrmapp.ui.config;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +17,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import hn.uth.minicrmapp.database.Cliente;
 import hn.uth.minicrmapp.databinding.FragmentConfigBinding;
 
 public class ConfigFragment extends Fragment {
@@ -22,14 +31,120 @@ public class ConfigFragment extends Fragment {
 
         binding = FragmentConfigBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        parseImage(getArguments());
 
 
         return root;
+    }
+
+    private void parseImage(Bundle bundleArgs) {
+        if(bundleArgs != null){
+            try{
+                Bundle bundle = bundleArgs.getBundle("foto");
+                if(bundle != null){
+                    Bitmap imagen = (Bitmap) bundle.get("data");
+                    binding.imgFull.setImageBitmap(imagen);
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            try{
+                String rutaFoto = bundleArgs.getString("rutafoto");
+                if(rutaFoto != null){
+                    showFullImage(rutaFoto);
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
+
+    private void showFullImage(String ubicacionFotoTomada)  {
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        int screenWidth = DeviceDimensionsHelper.getDisplayWidth(this.getContext());
+
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.outWidth = screenWidth;
+        bmOptions.inPurgeable = true;
+
+        Bitmap image = BitmapFactory.decodeFile(ubicacionFotoTomada, bmOptions);
+        Bitmap rotatedBitmap = null;
+        try{
+            ExifInterface ei = new ExifInterface(ubicacionFotoTomada);
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_UNDEFINED);
+
+            switch(orientation) {
+
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotatedBitmap = rotateImage(image, 90);
+                    break;
+
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotatedBitmap = rotateImage(image, 180);
+                    break;
+
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotatedBitmap = rotateImage(image, 270);
+                    break;
+
+                case ExifInterface.ORIENTATION_NORMAL:
+                default:
+                    rotatedBitmap = image;
+            }
+        }catch(Exception e){
+            rotatedBitmap = image;
+            e.printStackTrace();
+        }
+
+
+        binding.imgFull.setImageBitmap(rotatedBitmap);
+
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    public static class DeviceDimensionsHelper {
+        // DeviceDimensionsHelper.getDisplayWidth(context) => (display width in pixels)
+        public static int getDisplayWidth(Context context) {
+            DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+            return displayMetrics.widthPixels;
+        }
+
+        // DeviceDimensionsHelper.getDisplayHeight(context) => (display height in pixels)
+        public static int getDisplayHeight(Context context) {
+            DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+            return displayMetrics.heightPixels;
+        }
+
+        // DeviceDimensionsHelper.convertDpToPixel(25f, context) => (25dp converted to pixels)
+        public static float convertDpToPixel(float dp, Context context) {
+            Resources r = context.getResources();
+            return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics());
+        }
+
+        // DeviceDimensionsHelper.convertPixelsToDp(25f, context) => (25px converted to dp)
+        public static float convertPixelsToDp(float px, Context context) {
+            Resources r = context.getResources();
+            DisplayMetrics metrics = r.getDisplayMetrics();
+            float dp = px / (metrics.densityDpi / 160f);
+            return dp;
+        }
+
     }
 }
--- app/src/main/java/hn/uth/minicrmapp/ui/crm/ClientesFragment.java
+++ app/src/main/java/hn/uth/minicrmapp/ui/crm/ClientesFragment.java
@@ -1,19 +1,32 @@
 package hn.uth.minicrmapp.ui.crm;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
@@ -21,7 +34,12 @@ import androidx.navigation.Navigation;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import hn.uth.minicrmapp.R;
@@ -32,12 +50,16 @@ import hn.uth.minicrmapp.entity.Contacto;
 public class ClientesFragment extends Fragment {
 
     private static final int PERMISSION_REQUEST_READ_CONTACT = 700;
+    private static final int REQUEST_IMAGE_CAPTURE = 600;
     private FragmentClientesBinding binding;
     private Cliente clienteEditar;
+    private Intent intentFoto;
+    private String ubicacionFotoTomada;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ClientesViewModel clientesViewModel = new ViewModelProvider(this).get(ClientesViewModel.class);
-
+        ubicacionFotoTomada = null;
+        intentFoto = null;
         binding = FragmentClientesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
@@ -91,7 +113,127 @@ public class ClientesFragment extends Fragment {
             navController.navigate(R.id.navigation_home);
         } );
 
+        binding.imgFoto.setOnClickListener(v -> showImage());
+        binding.btnTomarFoto.setOnClickListener( v-> abrirCamaraFotos());
+
         return root;
+    }
+
+    private void abrirCamaraFotos() {
+        Intent tomarFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(tomarFotoIntent.resolveActivity(this.getActivity().getPackageManager()) != null){
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+            }catch(IOException e){
+                e.printStackTrace();
+                Toast.makeText(this.getContext(), "No se pudo crear el archivo de la fotografia", Toast.LENGTH_LONG).show();
+            }
+            if(photoFile != null){
+                Uri photoUri = FileProvider.getUriForFile(this.getContext(), "hn.uth.minicrmapp.fileprovider",photoFile);
+                tomarFotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(tomarFotoIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String actualtime = new SimpleDateFormat("yyMMdd_HHmmss").format(new Date());
+        String imageName = "JPEG_"+actualtime+"_";
+        File storageDir = this.getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageName, ".jpg",storageDir);
+        ubicacionFotoTomada = image.getAbsolutePath();
+
+        return image;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //OBTENIENDO LA VISTA PREVIA DE LA FOTOGRAFIA (IMAGEN PEQUEÑA)
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            if(data == null){
+                showFullImage();
+            }else{
+                Bundle extra = data.getExtras();
+                Bitmap imagen = (Bitmap) extra.get("data");
+                binding.imgFoto.setImageBitmap(imagen);
+                intentFoto = data;
+            }
+        }
+    }
+
+    private void showFullImage()  {
+        int targetW = binding.imgFoto.getWidth();
+        int targetH = binding.imgFoto.getHeight();
+
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(photoW / targetW, photoH/targetH);
+
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap image = BitmapFactory.decodeFile(ubicacionFotoTomada, bmOptions);
+        Bitmap rotatedBitmap = null;
+        try{
+            ExifInterface ei = new ExifInterface(ubicacionFotoTomada);
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_UNDEFINED);
+
+            switch(orientation) {
+
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotatedBitmap = rotateImage(image, 90);
+                    break;
+
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotatedBitmap = rotateImage(image, 180);
+                    break;
+
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotatedBitmap = rotateImage(image, 270);
+                    break;
+
+                case ExifInterface.ORIENTATION_NORMAL:
+                default:
+                    rotatedBitmap = image;
+            }
+        }catch(Exception e){
+            rotatedBitmap = image;
+            e.printStackTrace();
+        }
+
+
+        binding.imgFoto.setImageBitmap(rotatedBitmap);
+
+    }
+
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
+
+    public void showImage(){
+        Bundle bundle = new Bundle();
+        if(intentFoto != null) {
+            bundle.putBundle("foto", intentFoto.getExtras());
+            NavController navController = Navigation.findNavController(this.getActivity(), R.id.nav_host_fragment_activity_main);
+            navController.navigate(R.id.navigation_config, bundle);
+        }else if(ubicacionFotoTomada != null){
+            bundle.putString("rutafoto", ubicacionFotoTomada);
+            NavController navController = Navigation.findNavController(this.getActivity(), R.id.nav_host_fragment_activity_main);
+            navController.navigate(R.id.navigation_config, bundle);
+        }else{
+            Toast.makeText(this.getContext(), "No hay ninguna foto configurada", Toast.LENGTH_LONG).show();
+        }
     }
 
     private void mostrarClienteEditar() {
